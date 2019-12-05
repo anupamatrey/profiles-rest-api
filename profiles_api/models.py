9a4277dec4ada956f1c666704830030e8e518f44
Sharma,Anupam <anupam.sharma@metlife.com>
2019-12-05 16:05:19 -0500

    Added custom user profile mode , manager and migration

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,9 +1,34 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
 
 # Create your models here.
+class UserProfileManager(BaseUserManager):
+    """Manager for User profile"""
+
+    def create_user(self, name, email, password=None):
+        """Creating a user profile"""
+        if not email:
+            raise ValueError('User must have an email address')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        """Create and Save a new super user with given details"""
+        user = self.create_user(email, name, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database model fro users in the system"""
     email = models.EmailField(max_length=255, unique=True)
