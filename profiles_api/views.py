3b6e97a9acee5e769921fbe3f207574fd33d465d
Sharma,Anupam <anupam.sharma@metlife.com>
2019-12-18 15:54:17 -0500

    Add Permission

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,15 +1,19 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from profiles_api import serializers
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+
+from profiles_api import serializers
 from profiles_api import models
+from profiles_api import permission
 
 class UserProfileViewSet(viewsets.ModelViewSet):
     """Handle createing and updating profiles"""
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
-
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permission.UpdateOwnProfile,)
 
 
 class HelloViewSet(viewsets.ViewSet):
