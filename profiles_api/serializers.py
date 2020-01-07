c50b106a835f3239064144c864143872ed6570a0
Sharma <anupam.sharma@metlife.com>
2020-01-07 09:55:17 -0500

    Added user profile feed API

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -6,6 +6,15 @@ class HelloSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=20)
 
 
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    """Serializes profile feed items"""
+
+    class Meta:
+        model = models.ProfileFeedItem
+        fields = ('id', 'user_profile', 'status_text', 'created_on')
+        extra_kwargs = {'user_profile': {'read_only': True}}
+
+
 class UserProfileSerializer(serializers.ModelSerializer):
     """User profile object serializer"""
 
